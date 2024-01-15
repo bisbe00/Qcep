54614bce89cdb0248c76e007f614c514a110c1f0
bisbe00 <bisbe00@gmail.com>
2024-01-15 21:31:00 +0100

    header color

--- LOGIN/template/maindocuments.php
+++ LOGIN/template/maindocuments.php
@@ -1,29 +1,68 @@
-<main>
+<main class="proces">
     <?php
-        echo "<a href=\"?logged/connected\">BACK</a>";
         if(isset($resultProces) && count($resultProces) !== 0){
             foreach($resultProces as $proces){
-                echo "<h2>".$proces["nom"]."</h2>";
-                echo "<h3>Objectiu</h3>";
-            echo "<p class=\"text\">".$proces["objectiu"]."</p>";
-            if(isset($data) && count($data) !== 0){
-                echo "<table>";
-                echo "<tr>";
-                echo "<th>nom</th>";
-                echo "<th>link</th>";
-                echo "</tr>";
-                foreach($data as $document){
-                    echo "<tr>";
-                    echo "<td>".$document["nom"]."</td>";
-                    echo "<td><a href=\"\">".$document["link"]."</a></td>";
-                    echo "</tr>";
+                echo "<div class=\"proces1\">";
+                    echo "<a href=\"?logged/connected\">BACK</a>";
+                    echo "<h2>".$proces["nom"]."</h2>";
+                    echo "<h3>Objectiu</h3>";
+                    echo "<p class=\"text\">".$proces["objectiu"]."</p>";
+                    echo "<p><b>Author:</b> ".$proces["usuari_email"]."</p>";
+                echo "</div>";
+                if(isset($data) && count($data) !== 0){
+                    echo "<div>";
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<th>nom</th>";
+                        echo "<th>link</th>";
+                        echo "</tr>";
+                        foreach($data as $document){
+                            echo "<tr>";
+                            echo "<td>".$document["nom"]."</td>";
+                            echo "<td><a href=\"\">".$document["link"]."</a></td>";
+                            echo "</tr>";
+                        }
+                        echo  "</table>";
+                    echo "</div>";
                 }
-                echo  "</table>";
-            }else{
-                echo "<p>there is no documents</p>";
-            }
-            echo "<p><b>Author:</b> ".$proces["usuari_email"]."</p>";
             }
         }
     ?>
 </main>
+
+<!-- <main>
+    <div class="proces">
+        <?php
+            // if(isset($resultProces) && count($resultProces) !== 0){
+            //     foreach($resultProces as $proces){
+            //         echo "<a href=\"?logged/connected\">BACK</a>";
+            //         echo "<div>";
+            //             echo "<h2>".$proces["nom"]."</h2>";
+            //             echo "<h3>Objectiu</h3>";
+            //             echo "<p class=\"text\">".$proces["objectiu"]."</p>";
+            //             echo "<p><b>Author:</b> ".$proces["usuari_email"]."</p>";
+            //         echo "</div>";
+            //     if(isset($data) && count($data) !== 0){
+            //         echo "<div>";
+            //             echo "<table>";
+            //             echo "<tr>";
+            //             echo "<th>nom</th>";
+            //             echo "<th>link</th>";
+            //             echo "</tr>";
+            //             foreach($data as $document){
+            //                 echo "<tr>";
+            //                 echo "<td>".$document["nom"]."</td>";
+            //                 echo "<td><a href=\"\">".$document["link"]."</a></td>";
+            //                 echo "</tr>";
+            //             }
+            //             echo  "</table>";
+            //         echo "</div>";
+            //     }
+                // else{
+                //     echo "<p>there is no documents</p>";
+                // }
+            //     }
+            // }
+        ?>
+    </div>
+</main> -->
