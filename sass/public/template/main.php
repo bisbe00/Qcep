a14a558bcdd056f789c6bddd10fea8a38f914ddd
bisbe00 <bisbe00@gmail.com>
2024-03-12 20:44:06 +0100

    home colors

--- sass/public/template/main.php
+++ sass/public/template/main.php
@@ -1,3 +1,94 @@
 <main>
-    <h2>Hola</h2>
+
+    <p style="color:yellow;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:green;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:pink;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:cyan;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:cyan;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:violet;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:purple;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:indigo;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:darkblue;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:orange;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:brown;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:crimson;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
+    <p style="color:red;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem 
+        reprehenderit perferendis at repellat harum officiis, natus corrupti 
+        repellendus itaque ea doloribus, magnam quas doloremque vel, 
+        expedita architecto a voluptate! Accusamus!
+    </p>
+
 </main>
